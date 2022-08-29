5f433d5aedfc955acfb823acbc8d872c32a5a3e7
tin_pham <tin_pham@datahouse.com>
2022-08-29 20:10:52 +0700

    feat: override default style storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,12 @@
 import { addons } from "@storybook/addons";
 import customTheme from "./customTheme";
-
+import favicon from "./public/favicon.png";
+import "./titleAddon";
 addons.setConfig({
   theme: customTheme,
 });
+
+const link = document.createElement("link");
+link.setAttribute("rel", "shortcut icon");
+link.setAttribute("href", favicon);
+document.head.appendChild(link);
